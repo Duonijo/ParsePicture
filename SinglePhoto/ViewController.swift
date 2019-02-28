af3fd031de19969f475858201b5877fc5fd94650
HAXAIRE <nicolashaxaire@gmail.com>
2019-02-28 17:12:04 +0100

    First Commit

--- SinglePhoto/ViewController.swift
+++ SinglePhoto/ViewController.swift
@@ -7,14 +7,73 @@
 //
 
 import UIKit
+import Photos
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func importButton(_ sender: AnyObject) {
+        
+        let image = UIImagePickerController()
+        image.delegate = self
+        image.allowsEditing = false
+        image.sourceType = UIImagePickerController.SourceType.photoLibrary
+        
+        self.present(image, animated: true)
+        {
+            //after it is complete
+            print(image.title as Any)
+        }
+    }
+    
+    
+    func Autorize(){
+
+        switch PHPhotoLibrary.authorizationStatus(){
+            case .authorized:
+                NSLog("PHAuthorizationStatusAuthorized");
+                break;
+            case .denied:
+                NSLog("PHAuthorizationStatusDenied");
+                break;
+            case .restricted:
+                NSLog("PHAuthorizationStatusRestricted");
+                break;
+        case .notDetermined:
+            print("hello");
+            break;
+        }
+    }
+    
+    
+    private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        {
+            imageView.image = image
+            print("worked")
+        }
+        else{
+            //Error message
+            print("errooooooor")
+            
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
     override func viewDidLoad() {
+       
         super.viewDidLoad()
+         Autorize()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
 }
+
 
