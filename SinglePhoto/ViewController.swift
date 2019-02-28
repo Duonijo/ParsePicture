802222a2bd9aa0f0293d9d76064d6fb3ef6ea67e
HAXAIRE <nicolashaxaire@gmail.com>
2019-02-28 19:24:20 +0100

    Select a picture from camera or library

--- SinglePhoto/ViewController.swift
+++ SinglePhoto/ViewController.swift
@@ -14,66 +14,40 @@ import Photos
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
     @IBOutlet weak var imageView: UIImageView!
-    
-    @IBAction func importButton(_ sender: AnyObject) {
-        
-        let image = UIImagePickerController()
-        image.delegate = self
-        image.allowsEditing = false
-        image.sourceType = UIImagePickerController.SourceType.photoLibrary
-        
-        self.present(image, animated: true)
-        {
-            //after it is complete
-            print(image.title as Any)
-        }
-    }
-    
-    
-    func Autorize(){
-
-        switch PHPhotoLibrary.authorizationStatus(){
-            case .authorized:
-                NSLog("PHAuthorizationStatusAuthorized");
-                break;
-            case .denied:
-                NSLog("PHAuthorizationStatusDenied");
-                break;
-            case .restricted:
-                NSLog("PHAuthorizationStatusRestricted");
-                break;
-        case .notDetermined:
-            print("hello");
-            break;
-        }
-    }
-    
-    
-    private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
-        {
-            imageView.image = image
-            print("worked")
-        }
-        else{
-            //Error message
-            print("errooooooor")
-            
-        }
-        
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    
-    
     override func viewDidLoad() {
-       
         super.viewDidLoad()
-         Autorize()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBAction func importButton(_ sender: Any) {
+        print("j'ai cliqué")
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        
+        let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a photo", preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action:UIAlertAction) in
+            imagePickerController.sourceType = .camera
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Library", style: .default, handler: { (action:UIAlertAction) in
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        
+        self.present(actionSheet, animated: true, completion: nil)
 
-
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        imageView.image = image
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
 
 
